920e2f791989156958e28723f405d452efa08310
somnath89 <somnathsengupta845@yahoo.com>
2021-05-17 23:39:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 exports.handler = async (event) => {
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello from Lambda and Github!'),
+        body: JSON.stringify('Hello -- this is V3 of Lambda after Github update !'),
     };
     return response;
 };
